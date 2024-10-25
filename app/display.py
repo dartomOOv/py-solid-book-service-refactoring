053f5cbd9f97f3743a63922a0004b77264887c1a
Danyil <dartomov@gmail.com>
2024-10-25 17:02:40 +0200

    Remove extra comment

--- app/display.py
+++ app/display.py
@@ -17,11 +17,3 @@ class ConsoleDisplay(BaseDisplay):
 class ReverseDisplay(BaseDisplay):
     def display(self, book: Book) -> None:
         print(book.content[::-1])
-
-# def display(self, display_type: str) -> None:
-#     if display_type == "console":
-#         print(self.content)
-#     elif display_type == "reverse":
-#         print(self.content[::-1])
-#     else:
-#         raise ValueError(f"Unknown display type: {display_type}")
